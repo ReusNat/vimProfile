727f1c59af1c7005a3ed0c234368f2bd8399a544
Nathan <reusnat@bvu.edu>
2023-03-13 20:47:01 -0500

    Added html.vim

--- move.sh
+++ move.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 cp .vimrc ~/.vimrc
+if [ -e ~/.vim ] ; then
+    cp html.vim ~/.vim/indent/html.vim
+else
+    mkdir ~/.vim ~/.vim/indent
+    cp html.vim ~/.vim/indent/html.vim
+fi
 echo 'Vimrc moved, GLHF'
